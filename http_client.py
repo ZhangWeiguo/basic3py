557377c9f7181bc8143ae8d18f57846380cfcac2
zhangweiguo <zhangweiguo717@163.com>
2019-03-24 00:43:57 +0800

    Http Client

--- http_client.py
+++ http_client.py
@@ -1,13 +1,26 @@
-import requests
+from requests import Session
+from requests.cookies import merge_cookies
 
 
 class HttpClient:
     def __init__(self):
-        self.session = requests.Session()
-        self.headers = {}
+        self.__session = Session()
 
     def get(self, url, **kwargs):
-        return self.session.get(url, **kwargs)
+        return self.__session.get(url, **kwargs)
 
     def post(self, url, data, json = None, **kwargs):
-        return self.session.post(url, data, json, **kwargs)
+        return self.__session.post(url, data, json, **kwargs)
+
+    def get_headers(self):
+        return self.__session.headers
+
+    def set_headers(self, k, v):
+        self.__session.headers[k] = v
+
+    def get_cookies(self):
+        return self.__session.cookies
+
+    def set_cookies(self, cookies):
+        self.__session.cookies = merge_cookies(self.__session.cookies, cookies)
+
