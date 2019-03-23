23b0c5519e6398b525cb735aceb40e48f2a267df
zhangweiguo <zhangweiguo717@163.com>
2019-03-23 16:42:31 +0800

    多线程日志使用测试

--- concurrency.py
+++ concurrency.py
@@ -1,27 +1,43 @@
 import time
 import psutil,os
-from concurrent.futures import ThreadPoolExecutor,wait,ALL_COMPLETED
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import wait,ALL_COMPLETED,FIRST_COMPLETED,FIRST_EXCEPTION
 from concurrent.futures import ProcessPoolExecutor
 
 
 class MultiThreads(ThreadPoolExecutor):
     def __init__(self, max_workers = psutil.cpu_count()):
         super(MultiThreads, self).__init__(max_workers)
+        self.works = []
 
+    def submit(self, fn, *args, **kwargs):
+        fs = super(MultiThreads, self).submit(fn, *args, **kwargs)
+        self.works.append(fs)
+
+    def wait_all_completed(self, timeout = None):
+        wait(self.works, timeout = timeout, return_when = ALL_COMPLETED)
+    
+    def wait_first_completed(self, timeout = None):
+        wait(self.works, timeout = timeout, return_when = FIRST_COMPLETED)
+
+    def wait_first_exception(self, timeout = None):
+        wait(self.works, timeout = timeout, return_when = FIRST_EXCEPTION)
 
 class MultiProcess(ProcessPoolExecutor):
     def __init__(self, max_workers = psutil.cpu_count()):
         super(MultiProcess, self).__init__(max_workers)
+        self.works = []
+    
+    def submit(self, fn, *args, **kwargs):
+        fs = super(MultiProcess, self).submit(fn, *args, **kwargs)
+        self.works.append(fs)
 
+    def wait_all_completed(self, timeout = None):
+        wait(self.works, timeout = timeout, return_when = ALL_COMPLETED)
+    
+    def wait_first_completed(self, timeout = None):
+        wait(self.works, timeout = timeout, return_when = FIRST_COMPLETED)
 
+    def wait_first_exception(self, timeout = None):
+        wait(self.works, timeout = timeout, return_when = FIRST_EXCEPTION)
 
-if __name__ == "__main__":
-    def work():
-        pr = psutil.Process(os.getpid())
-        print("name(%10s) path(%20s) pid(%5d:%5d) Is Running"%(pr.name(), pr.cwd(), os.getpid(), os.getppid()))
-    MT = MultiThreads()
-    print("Max Workers: %d"%MT._max_workers)
-    fs = []
-    for i in range(10):
-        fs.append(MT.submit(work))
-    wait(fs, None, ALL_COMPLETED)
--- test/test_file_log.py
+++ test/test_file_log.py
@@ -1,8 +1,17 @@
 import sys
 sys.path.append("..")
-from file_log import LoggerCustom
+from file_log import LoggerCustom,Logger
+from concurrency import MultiThreads
+def log(f, j):
+    for i in range(10):
+        f.info("Thread %d: %d th log is happend"%(j, i))
 
-f = LoggerCustom("test")
-for i in range(1000):
-    f.log("%d th log is happend"%i)
-f.close()
+f = Logger("test", "test")
+Mt = MultiThreads(16)
+funs = []
+for i in range(100):
+    fun = Mt.submit(log, f, i)
+    funs.append(fun)
+Mt.wait_all_completed()
+
+
