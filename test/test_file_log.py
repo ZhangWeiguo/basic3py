e2e5ce643f447679df0ffe7378e67970a616e6bb
zhangweiguo <zhangweiguo717@163.com>
2019-03-23 19:55:38 +0800

    多线程日志测试

--- test/test_file_log.py
+++ test/test_file_log.py
@@ -1,17 +1,24 @@
-import sys
+import sys,time
 sys.path.append("..")
 from file_log import LoggerCustom,Logger
 from concurrency import MultiThreads
+
+N = 1000
+M = 16
 def log(f, j):
-    for i in range(10):
+    for i in range(N):
         f.info("Thread %d: %d th log is happend"%(j, i))
-
-f = Logger("test", "test")
-Mt = MultiThreads(16)
-funs = []
-for i in range(100):
-    fun = Mt.submit(log, f, i)
-    funs.append(fun)
-Mt.wait_all_completed()
-
+for thread_num in range(1, M + 1):
+    f = Logger("test", "test")
+    Mt = MultiThreads(thread_num)
+    t0 = time.time()
+    for i in range(M):
+        fun = Mt.submit(log, f, i)
+    Mt.wait_all_completed()
+    del Mt
+    print("%3d Threads: %d Log Cost %5.3f, Avg %d/S"%(
+        thread_num,
+        N * M, 
+        time.time() - t0, 
+        N * M / (time.time() - t0)))
 
